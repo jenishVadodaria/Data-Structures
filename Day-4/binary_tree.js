040195e0593dabff0e663e99efd4ecd9ce507fc9
jenishVadodaria <jenish.d.v@gmail.com>
2021-07-17 14:51:57 +0530

    First commit

---
+++ Day-4/binary_tree.js
@@ -0,0 +1,94 @@
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      console.log(`This is root element: ${this.root.data}`);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            console.log(`Left side node : ${node.left.data}`);
+            return;
+          } else if (node.left !== null) {
+            // console.log(`Left : ${this.node.left}`);
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            console.log(`Right side node : ${node.right.data}`);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      //   console.log(`${data}`);
+      return searchTree(node);
+    }
+  }
+
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        // node has no children
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // node has no left child
+        if (node.left == null) {
+          return node.right;
+        }
+        // node has no right child
+        if (node.right == null) {
+          return node.left;
+        }
+        // node has two children
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
+}
+
+const bst = new BST();
+
+bst.add(23);
+bst.add(20);
+bst.add(25);
+bst.add(30);
+bst.add(17);
+
+// bst.add(23);
+// bst.add(23);
