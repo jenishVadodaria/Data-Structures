ecb38371a88e316884564013806e33be8aa27959
jenishVadodaria <jenish.d.v@gmail.com>
2021-07-25 22:00:41 +0530

    First Commit

--- Day-4/binary_tree.js
+++ Day-4/binary_tree.js
@@ -21,7 +21,7 @@ class BST {
         if (data < node.data) {
           if (node.left === null) {
             node.left = new Node(data);
-            console.log(`Left side node : ${node.left.data}`);
+            console.log(`Left node : ${node.left.data}`);
             return;
           } else if (node.left !== null) {
             // console.log(`Left : ${this.node.left}`);
@@ -30,7 +30,7 @@ class BST {
         } else if (data > node.data) {
           if (node.right === null) {
             node.right = new Node(data);
-            console.log(`Right side node : ${node.right.data}`);
+            console.log(`Right node : ${node.right.data}`);
             return;
           } else if (node.right !== null) {
             return searchTree(node.right);
@@ -80,6 +80,84 @@ class BST {
     };
     this.root = removeNode(this.root, data);
   }
+
+  // Balanced BST
+  isBalanced() {
+    if (this.findMinHeight() >= this.findMaxHeight() - 1) {
+      console.log(`This tree is balanced`);
+    } else {
+      console.log(`This tree is not balanced`);
+    }
+    // return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+  findMinHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  findMaxHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+
+  // Traversal
+  inOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traverseInOrder(node) {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+  preOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePreOrder(node) {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+  postOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
 }
 
 const bst = new BST();
@@ -89,6 +167,11 @@ bst.add(20); //Left side node : 20
 bst.add(25); //Right side node : 25
 bst.add(30); //Right side node : 30
 bst.add(17); //Left side node : 17
+bst.isBalanced();
+
+console.log("inOrder: " + bst.inOrder());
+console.log("preOrder: " + bst.preOrder());
+console.log("postOrder: " + bst.postOrder());
 
 // bst.add(23);
 // bst.add(23);
